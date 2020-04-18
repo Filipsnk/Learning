06f6a1a65711518292962503c2cfce4cae44b1bf
FilipFraczek <pajakfilip@gmail.com>
2020-04-18 22:17:46 +0200

    Przegladniecie danych

--- All_state_kaggle.py
+++ All_state_kaggle.py
@@ -1,10 +1,25 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Sat Apr 18 21:15:55 2020
 
-@author: filipfraczek
-"""
+import pandas as pd
+import numpy as np
+import pandasql as sql
+
+data = pd.read_csv('train.csv')
+
+query = 'select * from data where Customer_ID in (10015208,10015208,10015221,10015226,10015288)'
+
+
+
+
+customer_data = sql.sqldf(query, locals())
+
+
+for i in data.columns:
+    print(i,round(data[i].isnull().sum()/len(data[i]),2))
+
+
+
 
 
 
